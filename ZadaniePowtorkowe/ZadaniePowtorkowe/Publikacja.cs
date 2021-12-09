a419005cd57296a84eb56706b777cda4ab9ad043
Sugar <sugarypolecat@gmail.com>
2021-12-09 21:08:55 +0100

    ionit

---
+++ ZadaniePowtorkowe/ZadaniePowtorkowe/Publikacja.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaniePowtorkowe
+{
+    internal class Publikacja
+    {
+        static int index;
+        static decimal cenaDodatkowa;
+        static string sygnatura;
+        static DateTime dataWydania;
+        static decimal cenaPodstawowa;
+
+        static void indeks(int ind, decimal cD)
+        {
+            index = ind;
+            cenaDodatkowa = cD;
+        }
+    }
+}
